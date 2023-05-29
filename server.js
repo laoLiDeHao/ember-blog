9eb25678fde58841b8835ad8243792f2f3864335
Ember.PL <1084861534@163.com>
2023-05-29 22:42:39 +0800

    1

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const history = require('connect-history-api-fallback');
-
-const app = express();
-app.use(history());
-app.use(express.static('build'));
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`App listening on port: ${port}`));
